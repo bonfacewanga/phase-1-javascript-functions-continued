a771d61b01ca4bf43d0fa1c51f459438ef9e6c66
Bonface <bonniewanga34@gmail.com>
2023-07-05 14:42:06 +0300

    changes in assignments

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+ }
+ 
+ function mondayWork(task = "go to the office") {
+ 
+    return `This Monday, I will ${task}.`;
+ 
+ }
+ 
+ function outer(greeting, msg = "It's a fine day to learn") {
+    
+    const innerFunction = function (name, lang = "Python") {
+       
+       return `${greeting}, ${name}! ${msg} ${lang}`; 
+    };
+ 
+    return innerFunction("student", "JavaScript"); 
+ }
+ 
+ 
+ function wrapAdjective(symbol = "*") {
+ 
+    return function inner(x= "special") {
+ 
+       return `You are ${symbol}${x}${symbol}!`;
+ 
+    };
+ }
+ 
+ 
